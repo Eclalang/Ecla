eba11a5282051643713ca295c39e19a26e795be7
tot0p <thomas.lemaitre.78420@gmail.com>
2022-10-26 12:39:15 +0200

    add main.go

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
-package ecla
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	Debug = false
+)
+
+func init() {
+	flag.BoolVar(&Debug, "debug", Debug, "enable debug mode")
+	flag.BoolVar(&Debug, "d", Debug, "enable debug mode (shorthand)")
+	flag.Parse()
+}
+
+func main() {
+	args := flag.Args()
+	if len(args) == 0 {
+		println("ecla: no input files")
+		return
+	}
+	fmt.Println("//--- RUN", args[0], "---")
+	// Run the interpreter
+}
