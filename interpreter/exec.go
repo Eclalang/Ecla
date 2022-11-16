db69295f898e857d94cd7565a60671dfaabdcfbd
mozaBit <bamhammedmetehri@gmail.com>
2022-11-16 11:23:12 +0000

    func Run(v1) exec.go | Bam

--- interpreter/exec.go
+++ interpreter/exec.go
@@ -1,5 +1,23 @@
 package interpreter
 
-func Run(env *Env) {
+import (
+	"fmt"
+)
 
+/*
+type Env struct {
+	Vars       map[string]eclaType.Type
+	OS         string
+	ARCH       string
+	SyntaxTree any
+	File       string
+	Code       string
+}
+*/
+
+func Run(env *Env) {
+	for _, v := range env.SyntaxTree.ParseTree.Operations {
+		fmt.Println(v)
+		fmt.Printf("type of Node %T\n", v)
+	}
 }
