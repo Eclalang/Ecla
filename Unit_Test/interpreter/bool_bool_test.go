29f2132cfa46b8cddf2904c1ce5c27f296f07123
Axou89 <senecal.axel89@gmail.com>
2022-11-30 10:14:07 +0100

    add unit test bool bool

---
+++ Unit_Test/interpreter/bool_bool_test.go
@@ -0,0 +1,70 @@
+package interpreter
+
+import (
+	"github.com/tot0p/Ecla/interpreter/eclaType"
+	"testing"
+)
+
+func TestEqBools(t *testing.T) {
+	t1 := eclaType.Bool(true)
+	t2 := eclaType.Bool(true)
+
+	result, err := t1.Eq(t2)
+	if err != nil {
+		t.Error(err)
+	}
+	if result.GetValue() != eclaType.Bool(true) {
+		t.Error("Expected true, got ", result)
+	}
+}
+
+func TestNotEqBools(t *testing.T) {
+	t1 := eclaType.Bool(true)
+	t2 := eclaType.Bool(false)
+
+	result, err := t1.NotEq(t2)
+	if err != nil {
+		t.Error(err)
+	}
+	if result.GetValue() != eclaType.Bool(true) {
+		t.Error("Expected true, got ", result)
+	}
+}
+
+func TestAndBools(t *testing.T) {
+	t1 := eclaType.Bool(true)
+	t2 := eclaType.Bool(true)
+
+	result, err := t1.And(t2)
+	if err != nil {
+		t.Error(err)
+	}
+	if result.GetValue() != eclaType.Bool(true) {
+		t.Error("Expected true, got ", result)
+	}
+}
+
+func TestOrBools(t *testing.T) {
+	t1 := eclaType.Bool(true)
+	t2 := eclaType.Bool(false)
+
+	result, err := t1.Or(t2)
+	if err != nil {
+		t.Error(err)
+	}
+	if result.GetValue() != eclaType.Bool(true) {
+		t.Error("Expected true, got ", result)
+	}
+}
+
+func TestNotBools(t *testing.T) {
+	t1 := eclaType.Bool(true)
+
+	result, err := t1.Not(t1)
+	if err != nil {
+		t.Error(err)
+	}
+	if result.GetValue() != eclaType.Bool(false) {
+		t.Error("Expected false, got ", result)
+	}
+}
