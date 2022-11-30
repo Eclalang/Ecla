5f3cb68c411559bbe706cd45f379ff64aa97a656
Axou89 <senecal.axel89@gmail.com>
2022-11-30 17:36:07 +0100

    add function version by @Axou89

--- interpreter/eclaKeyWord/func.go
+++ interpreter/eclaKeyWord/func.go
@@ -1,6 +1,9 @@
 package eclaKeyWord
 
-import "github.com/tot0p/Ecla/interpreter/eclaType"
+import (
+	"errors"
+	"github.com/tot0p/Ecla/interpreter/eclaType"
+)
 
 type Function struct {
 	Name  string
@@ -9,10 +12,21 @@ type Function struct {
 	Code  any
 }
 
-func (f *Function) GetType() eclaType.String {
+func (f *Function) GetType() string {
 	return "Function"
 }
 
-func (f *Function) Call() {
+func (f *Function) NewFunction(name string, args []eclaType.Type, scope []eclaType.Type, code any) *Function {
+	return &Function{Name: name, Args: args, Scope: scope, Code: code}
+}
+
+func (f *Function) Call(args []eclaType.Type) error {
 	//TODO EXECUTE CODE
+	if len(args) < len(f.Args) {
+		return errors.New("cannot call function " + f.Name + " too few arguments passed into")
+	}
+	if len(args) > len(f.Args) {
+		return errors.New("cannot call function " + f.Name + " too many arguments passed into")
+	}
+	return nil
 }
