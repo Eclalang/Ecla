cbd1480eae4ae4d009c694eaea33ce16a9f42404
Axou89 <senecal.axel89@gmail.com>
2022-11-30 10:47:05 +0100

    add unit test bool int

--- Unit_Test/interpreter/bool_int_test.go
+++ Unit_Test/interpreter/bool_int_test.go
@@ -17,3 +17,42 @@ func TestEqBoolInt(t *testing.T) {
 		t.Error("Expected true, got ", result)
 	}
 }
+
+func TestNotEqBoolInt(t *testing.T) {
+	t1 := eclaType.Bool(true)
+	t2 := eclaType.Int(1)
+
+	result, err := t1.NotEq(t2)
+	if err != nil {
+		t.Error(err)
+	}
+	if result.GetValue() != eclaType.Bool(false) {
+		t.Error("Expected false, got ", result)
+	}
+}
+
+func TestAndBoolInt(t *testing.T) {
+	t1 := eclaType.Bool(true)
+	t2 := eclaType.Int(1)
+
+	result, err := t1.And(t2)
+	if err != nil {
+		t.Error(err)
+	}
+	if result.GetValue() != eclaType.Bool(true) {
+		t.Error("Expected true, got ", result)
+	}
+}
+
+func TestOrBoolInt(t *testing.T) {
+	t1 := eclaType.Bool(true)
+	t2 := eclaType.Int(0)
+
+	result, err := t1.Or(t2)
+	if err != nil {
+		t.Error(err)
+	}
+	if result.GetValue() != eclaType.Bool(true) {
+		t.Error("Expected true, got ", result)
+	}
+}
