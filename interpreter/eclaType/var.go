ce61a56891a69fde9c01be7979a69944b3c47ac2
Luca Morgado <luca.morgado.pro@gmail.com>
2023-01-26 15:56:23 +0100

    fixed variable operation bug

--- interpreter/eclaType/var.go
+++ interpreter/eclaType/var.go
@@ -66,127 +66,85 @@ func (v *Var) SetVar(value Type) error {
 
 // Add adds two Type objects
 func (v *Var) Add(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.Add(other)
 }
 
 // Sub subtracts two Type objects
 func (v *Var) Sub(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.Sub(other)
 }
 
 // Mul multiplies two Type objects
 func (v *Var) Mul(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.Mul(other)
 }
 
 // Div divides two Type objects
 func (v *Var) Div(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.Div(other)
 }
 
 // Mod modulos two Type objects
 func (v *Var) Mod(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.Mod(other)
 }
 
 // DivEc divides two Type objects
 func (v *Var) DivEc(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.DivEc(other)
 }
 
 // Eq returns true if the two Type objects are equal
 func (v *Var) Eq(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.Eq(other)
 }
 
 // NotEq returns true if the two Type objects are not equal
 func (v *Var) NotEq(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.NotEq(other)
 }
 
 // Gt returns true if the first Type object is greater than the second
 func (v *Var) Gt(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.Gt(other)
 }
 
 // GtEq returns true if the first Type object is greater than or equal to the second
 func (v *Var) GtEq(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.GtEq(other)
 }
 
 // Lw returns true if the first Type object is lower than the second
 func (v *Var) Lw(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.Lw(other)
 }
 
 // LwEq returns true if the first Type object is lower than or equal to the second
 func (v *Var) LwEq(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.LwEq(other)
 }
 
 // And returns true if the two Type objects are true
 func (v *Var) And(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.And(other)
 }
 
 // Or returns true if either Type objects is true
 func (v *Var) Or(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.Or(other)
 }
 
@@ -212,10 +170,7 @@ func (v *Var) Increment() {
 }
 
 func (v *Var) Append(other Type) (Type, error) {
-	switch other.(type) {
-	case *Var:
-		return v.Value.Add(other.(*Var).Value)
-	}
+
 	return v.Value.Append(other)
 }
 
