db69cd19a90ba91b3006dfaacacafff1532dd13c
Luca Morgado <luca.morgado.pro@gmail.com>
2023-03-29 17:30:35 +0200

    updated operator precedence to fix precedence mistake for And and Or operators

--- parser/Precedence.go
+++ parser/Precedence.go
@@ -11,15 +11,15 @@ const (
 
 func TokenPrecedence(tok lexer.Token) int {
 	switch tok.TokenType {
-	case "temp1":
+	case lexer.OR:
 		return 1
-	case "temp2":
+	case lexer.AND:
 		return 2
 	case lexer.EQUAL, lexer.NEQ, lexer.LSS, lexer.LEQ, lexer.GTR, lexer.GEQ:
 		return 3
-	case lexer.ADD, lexer.SUB, lexer.OR:
+	case lexer.ADD, lexer.SUB:
 		return 4
-	case lexer.MULT, lexer.DIV, lexer.MOD, lexer.QOT, lexer.AND:
+	case lexer.MULT, lexer.DIV, lexer.MOD, lexer.QOT:
 		return 5
 	}
 	return LowestPrecedence
