6bb12ad3a16a9197e7c1253e544b22baf19b6393
Axou89 <senecal.axel89@gmail.com>
2023-04-25 22:47:49 +0200

    update encoding lib

--- interpreter/libs/encoding.go
+++ interpreter/libs/encoding.go
@@ -14,6 +14,14 @@ func NewEncoding() *Encoding {
 	return &Encoding{
 		functionMap: map[string]interface{}{
 			"asciiToString": nil,
+			"decodeBase64":  nil,
+			"decodeGob":     nil,
+			"decodeHex":     nil,
+			"decodeJSON":    nil,
+			"encodeBase64":  nil,
+			"encodeGob":     nil,
+			"encodeHex":     nil,
+			"encodeJSON":    nil,
 			"stringToAscii": nil,
 		},
 	}
@@ -35,6 +43,22 @@ func (e *Encoding) Call(name string, args []eclaType.Type) eclaType.Type {
 			arg2 = append(arg2, val.(int))
 		}
 		return utils.GoToEclaType(enc.AsciiToString(arg2))
+	case "decodeBase64":
+		// TODO : Fix this
+	case "decodeGob":
+		// TODO : Fix this
+	case "decodeHex":
+		// TODO : Fix this
+	case "decodeJSON":
+		// TODO : Fix this
+	case "encodeBase64":
+		// TODO : Fix this
+	case "encodeGob":
+		// TODO : Fix this
+	case "encodeHex":
+		// TODO : Fix this
+	case "encodeJSON":
+		// TODO : Fix this
 	case "stringToAscii":
 		return utils.GoToEclaType(enc.StringToAscii(newArgs[0].(string)))
 	default:
