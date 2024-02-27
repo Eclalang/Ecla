0b33462409d9a5caec71513d30063e01ae42d13f
Llelepipede <pchesneau3103@gmail.com>
2024-02-27 09:43:05 +0100

    with test

--- lexer/lexer_test_list.go
+++ lexer/lexer_test_list.go
@@ -682,10 +682,58 @@ var (
 		input: "b := \"hello\nworld\";",
 		output: []Token{
 			{
+				TokenType: TEXT,
+				Value:     `b`,
+				Position:  1,
+				Line:      1,
+			},
+			{
+				TokenType: COLON,
+				Value:     `:`,
+				Position:  3,
+				Line:      1,
+			},
+			{
+				TokenType: ASSIGN,
+				Value:     `=`,
+				Position:  4,
+				Line:      1,
+			},
+			{
+				TokenType: DQUOTE,
+				Value:     `"`,
+				Position:  6,
+				Line:      1,
+			},
+			{
+				TokenType: STRING,
+				Value:     "hello\n",
+				Position:  7,
+				Line:      1,
+			},
+			{
+				TokenType: TEXT,
+				Value:     `world`,
+				Position:  1,
+				Line:      2,
+			},
+			{
+				TokenType: DQUOTE,
+				Value:     `"`,
+				Position:  6,
+				Line:      2,
+			},
+			{
+				TokenType: STRING,
+				Value:     `;`,
+				Position:  7,
+				Line:      2,
+			},
+			{
 				TokenType: EOF,
 				Value:     ``,
-				Position:  18,
-				Line:      1,
+				Position:  8,
+				Line:      2,
 			},
 		},
 	}
