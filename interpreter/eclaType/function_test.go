ef8e7fe8d36e4a2326c1dae2cd90afe03653ae73
Sanegv <43825023+Sanegv@users.noreply.github.com>
2024-04-05 14:06:35 +0200

    implemented tests for new func & new anon func

--- interpreter/eclaType/function_test.go
+++ interpreter/eclaType/function_test.go
@@ -2,10 +2,143 @@ package eclaType
 
 import (
 	"github.com/Eclalang/Ecla/interpreter/eclaDecl"
+	"github.com/Eclalang/Ecla/lexer"
+	"github.com/Eclalang/Ecla/parser"
 	"testing"
 )
 
 // function interacts with function
+
+func TestGenerateArgsString(t *testing.T) {
+	var args []parser.FunctionParams
+	args = append(args, parser.FunctionParams{"arg0", "int"})
+	args = append(args, parser.FunctionParams{"arg1", "string"})
+
+	result := generateArgsString(args)
+	expected := "intstring"
+
+	if result != expected {
+		t.Errorf("Expected %s, got %s", expected, result)
+	}
+}
+
+func TestNewFunction(t *testing.T) {
+	var args []parser.FunctionParams
+	args = append(args, parser.FunctionParams{"arg0", "int"})
+	var ret []string
+	ret = append(ret, "string")
+	var body []parser.Node
+	body = append(
+		body, parser.Literal{
+			lexer.Token{
+				"type",
+				"val",
+				0,
+				0},
+			"type",
+			"val"})
+
+	f := NewFunction("test", args, body, ret)
+
+	//test allocation
+	if f == nil {
+		t.Errorf("Expected function, got %v", f)
+	}
+
+	//test name
+	if f.Name != "test" {
+		t.Error("Expected test, got " + f.Name)
+	}
+
+	//test args
+	if len(f.Args) != 1 {
+		t.Errorf("Expected exactly 1 set of arguments, got %d", len(f.Args))
+	}
+	for i, arg := range f.Args[0] {
+		if arg != args[i] {
+			t.Errorf("Expected %v, got %v", args[i], arg)
+		}
+	}
+
+	//test return
+	if len(f.Return) != 1 {
+		t.Errorf("Expected exactly 1 set of return types, got %d", len(f.Args))
+	}
+	for i, r := range f.Return["int"] {
+		if r != ret[i] {
+			t.Errorf("Expected %v, got %v", ret[i], r)
+		}
+	}
+
+	//test body
+	if len(f.Body) != 1 {
+		t.Errorf("Expected exactly 1 body, got %d", len(f.Args))
+	}
+	for i, b := range f.Body["int"] {
+		if b != body[i] {
+			t.Errorf("Expected %v, got %v", body[i], b)
+		}
+	}
+}
+
+func TestNewAnonymousFunction(t *testing.T) {
+	var args []parser.FunctionParams
+	args = append(args, parser.FunctionParams{"arg0", "int"})
+	var ret []string
+	ret = append(ret, "string")
+	var body []parser.Node
+	body = append(
+		body, parser.Literal{
+			lexer.Token{
+				"type",
+				"val",
+				0,
+				0},
+			"type",
+			"val"})
+
+	f := NewAnonymousFunction(args, body, ret)
+
+	//test allocation
+	if f == nil {
+		t.Errorf("Expected function, got %v", f)
+	}
+
+	//test name
+	if f.Name != "" {
+		t.Error("Expected \"\", got " + f.Name)
+	}
+
+	//test args
+	if len(f.Args) != 1 {
+		t.Errorf("Expected exactly 1 set of arguments, got %d", len(f.Args))
+	}
+	for i, arg := range f.Args[0] {
+		if arg != args[i] {
+			t.Errorf("Expected %v, got %v", args[i], arg)
+		}
+	}
+
+	//test return
+	if len(f.Return) != 1 {
+		t.Errorf("Expected exactly 1 set of return types, got %d", len(f.Args))
+	}
+	for i, r := range f.Return["int"] {
+		if r != ret[i] {
+			t.Errorf("Expected %v, got %v", ret[i], r)
+		}
+	}
+
+	//test body
+	if len(f.Body) != 1 {
+		t.Errorf("Expected exactly 1 body, got %d", len(f.Args))
+	}
+	for i, b := range f.Body["int"] {
+		if b != body[i] {
+			t.Errorf("Expected %v, got %v", body[i], b)
+		}
+	}
+}
 
 func TestGetType(t *testing.T) {
 	f := NewFunction("test", nil, nil, nil)
@@ -55,13 +188,6 @@ func TestIsNull(t *testing.T) {
 	f := NewFunction("test", nil, nil, nil)
 	if f.IsNull() {
 		t.Errorf("Expected false, got %v", f.IsNull())
-	}
-}
-
-func TestNewFunction(t *testing.T) {
-	f := NewFunction("test", nil, nil, nil)
-	if f == nil {
-		t.Errorf("Expected not nil, got %v", f)
 	}
 }
 
