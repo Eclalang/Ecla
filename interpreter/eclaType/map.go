f0f3950ee0bb1378ee7a839fdfb03c1d3a5ad2e3
Sanegv <43825023+Sanegv@users.noreply.github.com>
2024-05-26 22:28:06 +0200

    fixed the index

--- interpreter/eclaType/map.go
+++ interpreter/eclaType/map.go
@@ -297,7 +297,7 @@ func IsMap(typ string) bool {
 func GetTypeOfKeyAndValue(v string) (string, string) {
 	val := ""
 	count := 0
-	for i := 4; i < len(v); i++ {
+	for i := 3; i < len(v); i++ {
 		if v[i] == '[' {
 			count++
 		}
